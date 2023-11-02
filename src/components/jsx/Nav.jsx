c44bace11aa277db53f7ebb91f8f6ecf357af1be
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-02 14:05:23 -0600

    :memo: Add or update documentation

--- src/components/jsx/Nav.jsx
+++ src/components/jsx/Nav.jsx
@@ -18,20 +18,20 @@ const Nav = () => {
             className="bx bx-menu bx-flashing text-4xl"
           ></i>
         </div>
-        <div
-          className={`absolute top-full ${
-            isShow ? "left-0 z-50 w-[18rem]" : "-left-full"
-          } duration-300 flex flex-col gap-5 font-[600] bg-[#3f2a5d] pt-3 h-[20rem] w-full rounded-xl `}
-        >
-          <ul className="flex flex-col gap-5 p-5">
-            <li className="text-center">Features</li>
-            <li className="text-center">Pricing</li>
-            <li className="text-center">Resources</li>
-            <div className="border-b-2 border-b-gray-600"></div>
-            <li className="text-center">Login</li>
-            <ButtonSignUp />
-          </ul>
-        </div>
+      </div>
+      <div
+        className={`absolute top-full bg-[#3f2a5d] w-[18rem] ${
+          isShow ? "left-0 z-50 bg-blue-800" : "-left-full"
+        } transition-all duration-300 flex flex-col font-bold rounded-xl bg-red-400 `}
+      >
+        <ul className="flex flex-col gap-5 p-5">
+          <li className="text-center">Features</li>
+          <li className="text-center">Pricing</li>
+          <li className="text-center">Resources</li>
+          <div className="border-b-2 border-b-gray-600"></div>
+          <li className="text-center">Login</li>
+          <ButtonSignUp />
+        </ul>
       </div>
     </nav>
   );
