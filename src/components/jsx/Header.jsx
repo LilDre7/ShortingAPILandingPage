b3f15ac9065fe1a2a5bfbf08f6575a1a723a09ed
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-02 16:29:06 -0600

    :memo: Add or update documentation

--- src/components/jsx/Header.jsx
+++ src/components/jsx/Header.jsx
@@ -1,11 +1,29 @@
 // 🐋
 import Nav from "./Nav";
+import ButtonSignUp from "./ButtonSignUp";
 
 const header = () => {
   return (
-    <header>
+    <>
       <Nav />
-    </header>
+      <header className="text-white flex flex-col items-center justify-center w-full h-screen bg-[#F9FAFB] overflow-hidden">
+        <div className="">
+          <img
+            className="ml-[23px] w-[135%] max-w-[800px] "
+            src="/public/images/illustration-working.svg"
+            alt="Illustration Working"
+          />
+        </div>
+        <h1 className="text-4xl font-bold text-gray-900">
+          More than just shorter links
+        </h1>
+        <p className="mt-3 text-lg text-gray-600">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <ButtonSignUp />
+      </header>
+    </>
   );
 };
 
--- src/components/jsx/Nav.jsx
+++ src/components/jsx/Nav.jsx
@@ -9,22 +9,24 @@ const Nav = () => {
   };
 
   return (
-    <nav className="text-white absolute top-0 right-0 w-full">
-      <div className="flex justify-between p-3 mb-2 items-centerbg-red-700 h-14">
-        <h1 className="text-3xl font-bold tracking-wider">Shortly</h1>
+    <nav className="text-black absolute top-0 right-0 w-full">
+      <div className="flex justify-between p-3 mb-2 items-center h-14">
+        <div>
+          <img src="/public/images/logo.svg" alt="" />
+        </div>
         <div>
           <i
             onClick={handleClickShow}
-            className="bx bx-menu bx-flashing text-4xl"
+            className="bx bx-menu bx-flashing text-4xl text-black "
           ></i>
         </div>
       </div>
       <div
-        className={`absolute top-full bg-[#3f2a5d] w-[18rem] ${
-          isShow ? "left-0 z-50 bg-blue-800" : "-left-full"
-        } transition-all duration-300 flex flex-col font-bold rounded-xl bg-red-400 `}
+        className={`relative mx-auto bg-[#3b3054] w-[18rem] ${
+          isShow ? "left-0 z-50" : "-left-full"
+        } transition-all duration-300 flex flex-col font-bold rounded-xl pb-[2.5rem] pt-[1.5rem]`}
       >
-        <ul className="flex flex-col gap-5 p-5">
+        <ul className="flex flex-col gap-5 p-5 text-white text-[1.125rem] ">
           <li className="text-center">Features</li>
           <li className="text-center">Pricing</li>
           <li className="text-center">Resources</li>
