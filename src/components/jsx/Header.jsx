fddd179c79581f391e530d7d3471a19d4b9c9b15
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-03 13:31:29 -0600

    :memo: Add or update documentation

--- src/components/jsx/Header.jsx
+++ src/components/jsx/Header.jsx
@@ -6,7 +6,7 @@ const header = () => {
   return (
     <>
       <Nav />
-      <header className="text-white flex flex-col items-center justify-center w-full h-screen bg-[#F9FAFB] overflow-hidden">
+      <header className="text-white flex flex-col items-center justify-center w-full h-screen bg-[#F9FAFB] overflow-hidden  ">
         <div className="mb-3">
           <img
             className="ml-[23px] w-[135%] max-w-[800px] "
@@ -17,11 +17,13 @@ const header = () => {
         <h1 className="text-5xl font-bold text-gray-900 text-center mt-6">
           More than just shorter links
         </h1>
-        <p className="mt-3 px-4 text-lg text-gray-600">
+        <p className="mt-3 px-4 py-3 text-lg text-gray-600 text-center ">
           Build your brand's recognition and get detailed insights on how your
           links are performing.
         </p>
-        <ButtonSignUp />
+        <div className="">
+          <ButtonSignUp />
+        </div>
       </header>
     </>
   );
