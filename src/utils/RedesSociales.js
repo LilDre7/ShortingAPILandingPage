fd38a97f77ba138ee8d39209f831b691a48be926
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-06 16:41:18 -0600

    :construction: Work in progress

--- src/utils/RedesSociales.js
+++ src/utils/RedesSociales.js
@@ -2,18 +2,22 @@ const redesSociales = [
   {
     id: 1,
     img: "/public/images/icon-facebook.svg",
+    alt: "Facebook",
   },
   {
     id: 2,
     img: "/public/images/icon-twitter.svg",
+    alt: "Twitter",
   },
   {
     id: 3,
     img: "/public/images/icon-pinterest.svg",
+    alt: "Pinterest",
   },
   {
     id: 4,
     img: "/public/images/icon-instagram.svg",
+    alt: "Instagram",
   },
 ];
 
