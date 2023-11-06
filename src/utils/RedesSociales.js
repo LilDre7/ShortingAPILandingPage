4f41305150a5f07772ad21c280798cdef87925ae
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-06 14:38:20 -0600

    :construction: Work in progress

---
+++ src/utils/RedesSociales.js
@@ -0,0 +1,20 @@
+const redesSociales = [
+  {
+    id: 1,
+    img: "/public/images/icon-facebook.svg",
+  },
+  {
+    id: 2,
+    img: "/public/images/icon-twitter.svg",
+  },
+  {
+    id: 3,
+    img: "/public/images/icon-pinterest.svg",
+  },
+  {
+    id: 4,
+    img: "/public/images/icon-instagram.svg",
+  },
+];
+
+export default redesSociales;
