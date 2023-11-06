e4265fd43aef08e4cb2468cbafca2897c1ba5700
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-06 12:13:03 -0600

    :memo: Add or update documentation

--- src/components/jsx/CardCustom.jsx
+++ src/components/jsx/CardCustom.jsx
@@ -2,21 +2,22 @@ import infoCard from "../../utils/infoCard";
 
 const Card = () => {
   return (
-    <article>
+    <article className="absolute left-0 right-0 z-50 ">
       {infoCard.map((card, index) => (
         <CardCustom key={index} card={card} />
       ))}
+      <div className="w-[0.8rem] bg-violet-600 h-[27rem] absolute -z-10 left-0 right-0 top-[17rem] mx-auto"></div>
     </article>
   );
 };
 
 const CardCustom = ({ card }) => {
   return (
-    <section className="w-full flex flex-col justify-center items-center bg-red-500 h-screen">
+    <section className="w-full flex flex-col justify-center items-center h-[21rem] z-50 ">
       <div className="bg-white h-56 w-[310px] absolute rounded-lg shadow-lg shadow-gray-400">
         <div className="bg-violet-900 w-20 h-20 relative rounded-full mx-auto -translate-y-10">
           <img
-            className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            className="text-white relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
             src={card.img}
             alt={card.title}
           />
