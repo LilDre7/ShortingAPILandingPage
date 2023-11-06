d8615c9039ab78e85488d1d4eac740adaa9843b6
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-05 19:25:43 -0600

    :memo: Add or update documentation

--- src/components/jsx/CardCustom.jsx
+++ src/components/jsx/CardCustom.jsx
@@ -21,9 +21,13 @@ const CardCustom = () => {
 
   return (
     <div className=" w-full flex flex-col justify-center items-center bg-red-500 h-screen">
-      <div className="bg-white h-52 w-[310px] ">
-        <div className="bg-violet-900 flex justify-center">
-          <img src="/public/images/icon-brand-recognition.svg" alt="" />
+      <div className="bg-white h-56 w-[310px] absolute rounded-lg shadow-lg shadow-gray-400">
+        <div className="bg-violet-900 w-20 h-20 relative rounded-full mx-auto flex">
+          <img
+            className=""
+            src="/public/images/icon-brand-recognition.svg"
+            alt=""
+          />
         </div>
       </div>
     </div>
