4f67a508894bc984451c0f0a59c80aa164c2760a
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-11-05 20:57:14 -0600

    :memo: Add or update documentation

--- src/components/jsx/CardCustom.jsx
+++ src/components/jsx/CardCustom.jsx
@@ -18,33 +18,34 @@ const infoCard = [
   },
 ];
 
-const CardCustom = ({ infoCard }) => {
-  // Este es el array de las cartas 🚀
-
+const CardCustom = ({ card }) => {
   return (
-    <div className="w-full flex flex-col justify-center items-center bg-red-500 h-screen">
-      {infoCard.map((item, index) => (
-        <div
-          key={index}
-          className="bg-white h-56 w-[310px] absolute rounded-lg shadow-lg shadow-gray-400"
-        >
-          <div className="bg-violet-900 w-20 h-20 relative rounded-full mx-auto -translate-y-10">
-            <img
-              className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-              src={item.img}
-              alt=""
-            />
-          </div>
-          <div>
-            <h1 className="text-2xl font-bold text-center mt-5">
-              {item.title}
-            </h1>
-            <p className="text-center text-gray-500">{item.text}</p>
-          </div>
+    <section className="w-full flex flex-col justify-center items-center bg-red-500 h-screen">
+      <div className="bg-white h-56 w-[310px] absolute rounded-lg shadow-lg shadow-gray-400">
+        <div className="bg-violet-900 w-20 h-20 relative rounded-full mx-auto -translate-y-10">
+          <img
+            className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            src={card.img}
+            alt={card.title}
+          />
         </div>
-      ))}
-    </div>
+        <div>
+          <h1 className="text-2xl font-bold text-center mt-5">{card.title}</h1>
+          <p className="text-center text-gray-500">{card.text}</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default CardCustom;
+function Card() {
+  return (
+    <article>
+      {infoCard.map((card, index) => (
+        <CardCustom key={index} card={card} />
+      ))}
+    </article>
+  );
+}
+
+export default Card;
